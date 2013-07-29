9055e9f7b8ebf7f79aefda95520da6cc911bb9fb
Justin Lambert <jlambert@eml.cc>
2013-07-29 12:51:18 -0600

    initial password expire warning

---
+++ manifests/pwd_expire.pp
@@ -0,0 +1,26 @@
+class openldap::pwd_expire(
+  $uri                = 'ldap://localhost:389',
+  $rootdn             = 'cn=Manager,dc=example,dc=com',
+  $rootpw             = 'secret',
+  $defaultpwdpolicydn = 'cn=default,ou=pwpolicies,dc=example,dc=com',
+  $searchbase         = 'ou=people,dc=example,dc=com',
+  $searchfilter       = '(&(uid=*)(objectClass=inetOrgPerson))',
+  $searchscope        = 'one',
+  $searchbin          = '/usr/bin/ldapsearch',
+  $search_flags       = '-ZZ',
+  $name_attr          = 'cn',
+  $login_attr         = 'uid',
+  $mail_attr          = 'mail',
+  $mail_subject       = 'Your password is expiring soon',
+  $mail_body          = "%name - Please change your password\n\nThe LDAP team."
+) {
+
+  file { '/usr/local/bin/checkLdapPwdExpiration.sh':
+    ensure  => 'file',
+    mode    => '0550',
+    owner   => 'root',
+    group   => 'root',
+    content => template('openldap/checkLdapPwdExpiration.sh.erb'),
+  }
+
+}
